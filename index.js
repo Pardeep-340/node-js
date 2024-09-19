8f3c4a2a1a36f51c80d5d55f0b276d82f4e039bc
pardeep-340 <pardeepjangra514@gmail.com>
2024-09-19 12:12:52 +0530

    code merge

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const app = express()
+var fs = require('fs');
 
 app.get('/', (req, res) => {
     res.send('hello world')
 })
 
-app.listen(3000, '', () => {
-    console.log('node js is running', 3000)
+app.get('/', (req, res) => {
+    fs.readFile('demofile1.html', function (err, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+    })
+})
+
+app.listen(3000, () => {
+    console.log(3000)
 })
